f21f199eaa1217bf6100f8e288c66082b8e80271
Fragtality <78614902+Fragtality@users.noreply.github.com>
2025-10-12 00:38:28 +0200

    Fix: Rolled Back NeoLua Version to fix Issues with Lua Plugins (e.g. iniA350)

--- Installer/Properties/AssemblyInfo.cs
+++ Installer/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Windows;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("Any2GSX Installer v0.1.20.0 (2025.10.04.2013)")]
+[assembly: AssemblyTitle("Any2GSX Installer v0.1.20.0 (2025.10.11.2202)")]
 [assembly: AssemblyDescription("Installer Application for Any2GSX")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Fragtality")]
