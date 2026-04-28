321e745756fcd250d20bd9dbcfafa7c86b399c42
Fragtality <78614902+Fragtality@users.noreply.github.com>
2026-04-28 23:01:31 +0200

    Changelog too long oO

--- Installer/Properties/AssemblyInfo.cs
+++ Installer/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Windows;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("Any2GSX Installer v0.4.0.0 (2026.04.28.2030)")]
+[assembly: AssemblyTitle("Any2GSX Installer v0.4.0.0 (2026.04.28.2059)")]
 [assembly: AssemblyDescription("Installer Application for Any2GSX")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Fragtality")]
