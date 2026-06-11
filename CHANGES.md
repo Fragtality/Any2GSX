d70f61198aa453433b63fd7017c5c551d9070607
Fragtality <78614902+Fragtality@users.noreply.github.com>
2026-06-11 14:00:49 +0200

    Push Version

--- CHANGES.md
+++ CHANGES.md
@@ -4,3 +4,7 @@
 - Added Checks to detect externally answered FollowMe Question (although Skip FollowMe is enabled in A2G)
 - Added some more Log Messages
 - Fixed Operator Selection being skipped and Menu being reset because of previous Operator Selection in certain Situations
+- Updated NuGet Packages
+
+### Installer
+- Set .NET 10.0.9 as Target
--- Installer/Properties/AssemblyInfo.cs
+++ Installer/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Windows;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("Any2GSX Installer v0.4.9.0 (2026.06.08.2003)")]
+[assembly: AssemblyTitle("Any2GSX Installer v0.4.10.0 (2026.06.11.1159)")]
 [assembly: AssemblyDescription("Installer Application for Any2GSX")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Fragtality")]
@@ -46,5 +46,5 @@ using System.Windows;
 //      Buildnummer
 //      Revision
 //
-[assembly: AssemblyVersion("0.4.9.0")]
-[assembly: AssemblyFileVersion("0.4.9.0")]
+[assembly: AssemblyVersion("0.4.10.0")]
+[assembly: AssemblyFileVersion("0.4.10.0")]
