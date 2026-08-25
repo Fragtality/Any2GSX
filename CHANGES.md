b4055911d4aa1696dbca905e38352bd8f9ff6177
Fragtality <78614902+Fragtality@users.noreply.github.com>
2026-08-25 18:09:27 +0200

    Push Build

---
+++ CHANGES.md
@@ -0,0 +1,2 @@
+### App
+- Fixed Text on Ground Equipment Page
--- Installer/Properties/AssemblyInfo.cs
+++ Installer/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Windows;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("Any2GSX Installer v0.4.12.0 (2026.08.24.1700)")]
+[assembly: AssemblyTitle("Any2GSX Installer v0.4.12.0 (2026.08.25.1608)")]
 [assembly: AssemblyDescription("Installer Application for Any2GSX")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Fragtality")]
